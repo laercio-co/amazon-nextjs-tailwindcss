b72d4a7a8a0c9b40ebc06ceb7131480bb8a33a5d
Laercio Nascimento <laercio@protonmail.ch>
2021-07-29 21:39:28 -0300

    initialize commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        colors: {
+          amazon_blue: {
+            light: '#232F3E',
+            DEFAULT: '#131921',
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
